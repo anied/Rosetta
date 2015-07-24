73012db1775e8772d56bef61d100d08fd01067bd
Alexander Nied <awnied@gmail.com>
2015-07-24 01:23:47 -0500

    Added the merging piece.  Still super rough-- refactoring will be needed.

--- rosetta.py
+++ rosetta.py
@@ -5,8 +5,8 @@ from collections import OrderedDict
 
 serial = 0
 
-def generate_translation_docs(source_file):
 
+def generate_translation_docs(source_file):
 
     def transliterator(src, trans):  # source_obj, translation_obj
 
@@ -79,3 +79,37 @@ def generate_translation_docs(source_file):
 
         rosetta_file.write(json.dumps(main_contents))
         translation_file.write(json.dumps(translation_contents))
+
+
+def merge_translation(translation_doc, rosetta_doc, language):
+
+    def converge_entry(src, trans):
+        if isinstance(src, dict):
+            for key in src:
+                if isinstance(key, dict):
+                    converge_entry(key, trans)
+                elif isinstance(src[key], dict) or isinstance(src[key], list):
+                    converge_entry(src[key], trans)
+                else:
+                    src[key] = trans[src[key]]
+        if isinstance(src, list):
+            for x in xrange(0, len(src)):
+                if isinstance(src[x], dict):
+                    converge_entry(src[x], trans)
+                else:
+                    src[x] = trans[src[x]]
+
+    # gets rosetta basename then strips off the "-rosetta.jon" to get true basename
+    file_basename = os.path.splitext(os.path.basename(rosetta_doc))[0]
+    file_basename = file_basename[0:len(file_basename)-len("-rosetta")]
+    translated_json_doc = file_basename+"-"+language+".json"
+
+    with open(translation_doc, 'r') as translations, open(rosetta_doc, 'r') as rosetta, open(translated_json_doc, 'w') as merge_doc:
+
+        translated_content = json.JSONDecoder(object_pairs_hook=OrderedDict).decode(translations.read())
+
+        rosetta_stone = json.JSONDecoder(object_pairs_hook=OrderedDict).decode(rosetta.read())
+
+        converge_entry(rosetta_stone, translated_content)
+
+        merge_doc.write(json.dumps(rosetta_stone))
---
+++ rosetta_merge_test.py
@@ -0,0 +1,3 @@
+import rosetta
+
+rosetta.merge_translation('example_complex-translated-sp.json', 'example_complex-rosetta.json', 'spanish')
